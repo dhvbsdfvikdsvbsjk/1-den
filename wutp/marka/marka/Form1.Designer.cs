1607d81a66320fb45101741a281feba36032ec67
dhvbsdfvikdsvbsjk <92776834+dhvbsdfvikdsvbsjk@users.noreply.github.com>
2022-04-01 18:44:24 +0500

    Add files via upload

---
+++ wutp/marka/marka/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace marka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CheckMark";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "MarkAfter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(30, 201);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "AfterRange";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(150, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(206, 188);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(206, 227);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(30, 258);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Countln";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(419, 89);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "CheckVin";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(419, 143);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "vincountry";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(419, 201);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "transportYo";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(150, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(573, 304);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(573, 350);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(573, 401);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "label7";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(409, 42);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(188, 20);
+            this.textBox4.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(573, 247);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(436, 247);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Кол-во символов:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(436, 304);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(119, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Проверка VIN номера";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(436, 350);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(118, 13);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Географическая зона";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(445, 401);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(110, 13);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Год когда запущено";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(30, 304);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 23;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(30, 350);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Задание 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ wutp/marka/marka/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using REG_MARK_LIB;
+using VIN_LIB;
+
+namespace marka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Moibjak a = new Moibjak();  // ввод класса библиотеки для вывода метода 
+            bool ex = a.CheckMark(textBox1.Text); 
+            if(ex == true)
+            {
+                label1.Text = textBox1.Text;
+            }
+            else
+            {
+                MessageBox.Show("Не правильно");
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Moibjak a = new Moibjak(); // ввод класса библиотеки для вывода метода 
+            string l = a.GetNextMarkAfter(textBox1.Text);
+            string c = textBox1.Text.Substring(0, 6);
+            string j = l.Substring(1,3);
+            int i = Convert.ToInt32(j);
+            int by = i + 1;
+            if(i == 999)
+            {
+                label2.Text = textBox1.Text.Replace(c, "а452сн");
+            }
+            else if (i == 000)
+            {
+                MessageBox.Show("Номер сериии равен 0");
+            }
+            else
+            {
+                    label2.Text = by.ToString();              
+            }  
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Moibjak a = new Moibjak(); // ввод класса библиотеки для вывода метода 
+            string ge;
+            string h = textBox2.Text;
+            string g = textBox3.Text;
+            string l = a.GetNextMarkAfterRange(textBox1.Text, textBox2.Text, textBox3.Text);
+            string ytq = textBox1.Text.Substring(1, 3);
+            int h1 = Convert.ToInt32(textBox2.Text);
+            int h2 = Convert.ToInt32(textBox3.Text);
+            int yte = Convert.ToInt32(ytq);
+            if(h1 > yte || yte < h2)
+            {
+                    ge = (yte + h1).ToString(); 
+                    label3.Text = ge;
+            }
+            else if (h2 < yte)
+            {
+                label3.Text = "out of stock";
+            }
+            else
+            {
+                MessageBox.Show("out of stock");
+            }
+
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Kle a = new Kle(); // ввод класса библиотеки для вывода метода 
+            bool l = a.CheckVIN(textBox4.Text);
+            int bv = textBox4.TextLength;
+            string hf= bv.ToString();
+            if (l == true)
+            {
+                label5.Text = textBox4.Text;
+                label8.Text=hf;
+            }
+            else
+            {
+                MessageBox.Show("Не правильно");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Kle a = new Kle(); // ввод класса библиотеки для вывода метода 
+            string l = a.GetVINCountry(textBox4.Text);
+            string buv = l.Substring(0, 2);
+            if(buv == "SA" || buv == "SM" || buv == "SN" || buv == "ST" || buv == "SU" || buv == "SZ" || buv == "S1" || buv == "S4" || buv == "TA"
+                || buv == "TH" || buv == "TJ" || buv == "TP" || buv == "TR" || buv == "TV" || buv == "TW" || buv == "T1" || buv == "T2" || buv == "T0" || buv == "UA"
+                || buv == "UG" || buv == "UH" || buv == "UM" || buv == "UN" || buv == "UT" || buv == "UU" || buv == "UZ" || buv == "U1" || buv == "U4" || buv == "U5" || buv == "U7"
+                || buv == "U8" || buv == "U0" || buv == "VA" || buv == "VE" || buv == "VF" || buv == "VR" || buv == "VS" || buv == "VW" || buv == "VX" || buv == "V2" || buv == "V3" || buv == "V5" || buv == "V6"
+                || buv == "V0" || buv == "WA" || buv == "W0" || buv == "XA" || buv == "XE" || buv == "XF" || buv == "XK" || buv == "XL" || buv == "XR" || buv == "XS"
+                || buv == "XW" || buv == "XX" || buv == "X2" || buv == "X3" || buv == "X0" || buv == "YA" || buv == "YE" || buv == "YF" || buv == "YK" || buv == "YL"
+                || buv == "YR" || buv == "YS" || buv == "YW" || buv == "YX" || buv == "Y2" || buv == "Y3" || buv == "Y5" || buv == "Y6" || buv == "Y0" || buv == "ZA"
+                || buv == "ZR" || buv == "ZS" || buv == "ZW" || buv == "ZX" || buv == "Z2" || buv == "Z3" || buv == "Z5" || buv == "Z6" || buv == "Z0")
+            {
+                label6.Text = "Европа";
+            }
+            else if(buv == "AA" || buv == "AH" || buv == "AJ" || buv == "AP" || buv == "A0" || buv == "BA" || buv == "BE" || buv == "BF" || buv == "BK"
+                || buv == "BL" || buv == "BR" || buv == "BS" || buv == "B0" || buv == "CA" || buv == "CE" || buv == "CF" || buv == "CK" || buv == "CL" || buv == "CR"
+                || buv == "CS" || buv == "C0" || buv == "DA" || buv == "DE" || buv == "DF" || buv == "DK" || buv == "DL" || buv == "DR" || buv == "DS" || buv == "D0" || buv == "EA"
+                || buv == "EE" || buv == "EF" || buv == "EK" || buv == "EL" || buv == "E0" || buv == "FA" || buv == "FE" || buv == "FF" || buv == "FK" || buv == "FL" || buv == "F0" || buv == "GA" || buv == "G0"
+                || buv == "HA" || buv == "H0" )
+            {
+                label6.Text = "Африка";
+            }
+            else if (buv == "JA" || buv == "JT" || buv == "KA" || buv == "KE" || buv == "KF" || buv == "KK" || buv == "KL" || buv == "KR" || buv == "KS"
+                || buv == "K0" || buv == "LA" || buv == "L0" || buv == "MA" || buv == "ME" || buv == "MF" || buv == "MK" || buv == "ML" || buv == "MR" || buv == "MS"
+                || buv == "M0" || buv == "NF" || buv == "NK" || buv == "NL" || buv == "NR" || buv == "NT" || buv == "N0" || buv == "PA" || buv == "PE" || buv == "PF" || buv == "PK"
+                || buv == "PL" || buv == "PR" || buv == "PS" || buv == "P0" || buv == "RA" || buv == "RE" || buv == "RF" || buv == "RK" || buv == "RL" || buv == "RR" || buv == "RS" || buv == "R0")
+            {
+                label6.Text = "Азия";
+            }
+            else if (buv == "1A" || buv == "10" || buv == "2A" || buv == "20" || buv == "3A" || buv == "3W" || buv == "3X" || buv == "37" || buv == "38"
+                || buv == "30" || buv == "4A" || buv == "40" || buv == "5A" || buv == "50")
+            {
+                label6.Text = "Северная Америка";
+            }
+            else if (buv == "8A" || buv == "8E" || buv == "8F" || buv == "8K" || buv == "8L" || buv == "8R" || buv == "8S" || buv == "8W" || buv == "8X"
+                || buv == "82" || buv == "83" || buv == "80" || buv == "9A" || buv == "9E" || buv == "9F" || buv == "9K" || buv == "9L" || buv == "9R" || buv == "9S"
+                || buv == "9W" || buv == "9X" || buv == "92" || buv == "93" || buv == "99" || buv == "90")
+            {
+                label6.Text = "Южная Америка";
+            }
+            else if (buv == "6A" || buv == "6W" || buv == "6X" || buv == "60" || buv == "7A" || buv == "7E" || buv == "7F" || buv == "70")
+            {
+                label6.Text = "Океания";
+            }
+            else
+            {
+                MessageBox.Show("Такого региона нет");
+            }
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Kle a = new Kle(); // ввод класса библиотеки для вывода метода 
+            string l = a.GetTransportYear(textBox4.Text);
+            string buv = l.Substring(10);
+            if(buv == "A" || buv == "B" || buv == "C" || buv == "D" || buv == "E" || buv == "F" || buv == "G" || buv == "H" || buv == "J" || buv == "K" || buv == "L" || buv == "M" || buv == "N" || buv == "P" || buv == "R"
+                || buv == "S" || buv == "T" || buv == "V" || buv == "W" || buv == "X" || buv == "Y")
+            {
+                label7.Text = "Автомобиль выпущен раньее 2000 года";
+            }
+            else if(buv == "1")
+            {
+                label7.Text = "Автомобиль выпущен в 2001 году";
+            }
+            else if (buv == "2")
+            {
+                label7.Text = "Автомобиль выпущен в 2002 году";
+            }
+            else if (buv == "3")
+            {
+                label7.Text = "Автомобиль выпущен в 2003 году";
+            }
+            else if (buv == "4")
+            {
+                label7.Text = "Автомобиль выпущен в 2004 году";
+            }
+            else if (buv == "5")
+            {
+                label7.Text = "Автомобиль выпущен в 2005 году";
+            }
+            else if (buv == "6")
+            {
+                label7.Text = "Автомобиль выпущен в 2006 году";
+            }
+            else if (buv == "7")
+            {
+                label7.Text = "Автомобиль выпущен в 2007 году";
+            }
+            else if (buv == "8")
+            {
+                label7.Text = "Автомобиль выпущен в 2008 году";
+            }
+            else if (buv == "9")
+            {
+                label7.Text = "Автомобиль выпущен в 2009 году";
+            }
+            else if (buv == "A")
+            {
+                label7.Text = "Автомобиль выпущен в 2010 году";
+            }
+            else if (buv == "B")
+            {
+                label7.Text = "Автомобиль выпущен в 2011 году";
+            }
+            else if (buv == "C")
+            {
+                label7.Text = "Автомобиль выпущен в 2012 году";
+            }
+            else if (buv == "D")
+            {
+                label7.Text = "Автомобиль выпущен в 2013 году";
+            }
+            else if (buv == "E")
+            {
+                label7.Text = "Автомобиль выпущен в 2014 году";
+            }
+            else if (buv == "F")
+            {
+                label7.Text = "Автомобиль выпущен в 2015 году";
+            }
+            else if (buv == "G")
+            {
+                label7.Text = "Автомобиль выпущен в 2016 году";
+            }
+            else if (buv == "T")
+            {
+                label7.Text = "Автомобиль выпущен в 2017 году";
+            }
+            else if (buv == "J")
+            {
+                label7.Text = "Автомобиль выпущен в 2018 году";
+            }
+            else if (buv == "K")
+            {
+                label7.Text = "Автомобиль выпущен в 2019 году";
+            }
+            else if (buv == "L")
+            {
+                label7.Text = "Автомобиль выпущен в 2020 году";
+            }
+            else if (buv == "M")
+            {
+                label7.Text = "Автомобиль выпущен в 2021 году";
+            }
+            else
+            {
+                label7.Text = "Автомобиль выпущен раньше 2022 года";
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Moibjak a = new Moibjak();
+            int l = a.GetCombinationsCountlnRange(textBox5.Text, textBox6.Text);
+            label4.Text = l.ToString();
+        }
+    }
+}
